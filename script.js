d478cea3ce1738942cd4fbd23a450706015c04b7
ronnyfesantos <106789851+ronnyfesantos@users.noreply.github.com>
2023-05-24 23:48:52 -0300

    Upload do projeto

---
+++ script.js
@@ -0,0 +1,29 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu')
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open')
+    }
+
+}
+
+function menuForm() {
+    let menuMobile = document.querySelector('.mobile-menu')
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open')
+    }
+
+}
+
+function menuExp() {
+    let menuMobile = document.querySelector('.mobile-menu')
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open')
+    }
+
+}
